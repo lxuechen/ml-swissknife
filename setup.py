de83d91263c8e157042f21c04a023e3a50a018f3
Xuechen Li <12689993+lxuechen@users.noreply.github.com>
2021-06-27 15:15:35 -0700

    First version.

---
+++ setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+version = "0.0.1"
+
+setuptools.setup(
+    name="swissknife",
+    version=version,
+    author="Xuechen Li",
+    author_email="lxuechen@cs.toronto.edu",
+    description="Reusable ML research primitives for fast prototyping",
+    url="https://github.com/lxuechen/swissknife",
+    packages=setuptools.find_packages(exclude=[]),
+    install_requires=['torch>=1.6.0'],
+    python_requires='~=3.6',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+    ],
+)
