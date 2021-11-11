8149b3612881931fe7082774bf1a4f0fb4372963
Xuechen Li <12689993+lxuechen@users.noreply.github.com>
2021-11-11 01:07:47 -0800

    Fix plots.

--- experiments/rr_vs_laplace/main.py
+++ experiments/rr_vs_laplace/main.py
@@ -31,7 +31,7 @@ def rr(x, epsilon):
 
     a = (np.e ** epsilon + 1) / (np.e ** epsilon - 1)
     b = 1 / (np.e ** epsilon - 1)
-    return (a * z - b).mean()
+    return (a * z - b).mean().item()
 
 
 def laplace(x, epsilon):
@@ -40,33 +40,53 @@ def laplace(x, epsilon):
     dist = distributions.laplace.Laplace(loc=loc, scale=scale)
     w = dist.sample()
     assert x.size() == w.size()
-    return (x + w).mean()
+    return (x + w).mean().item()
 
 
 def _squared_error(x, mean):
     return (x - mean) ** 2
 
 
-def _make_mse_estimates(ns, t, epsilon, sample_x):
+def _make_estimates(ns, t, epsilon, sample_x):
     mse_rr = []
     mse_lp = []
+
+    mean_rr = []
+    mean_lp = []
+
+    ci_rr = []
+    ci_lp = []
     for n in ns:
         err_rr = []
         err_lp = []
+
+        x_rr_l = []
+        x_lp_l = []
         for _ in tqdm.tqdm(range(t), desc="T"):
             x, true_mean = sample_x(n)
 
             x_rr = rr(x, epsilon=epsilon)
-            assert x_rr.dim() == 0
-            err_rr.append(_squared_error(x_rr, mean=true_mean).item())
+            err_rr.append(_squared_error(x_rr, mean=true_mean))
 
             x_lp = laplace(x, epsilon=epsilon)
-            assert x_lp.dim() == 0
-            err_lp.append(_squared_error(x_lp, mean=true_mean).item())
+            err_lp.append(_squared_error(x_lp, mean=true_mean))
+
+            x_rr_l.append(x_rr)
+            x_lp_l.append(x_lp)
 
         mse_rr.append(torch.tensor(err_rr).mean().item())
         mse_lp.append(torch.tensor(err_lp).mean().item())
-    return mse_rr, mse_lp
+
+        ci_rr.append(_ci(x_rr_l))
+        ci_lp.append(_ci(x_lp_l))
+
+        mean_rr.append(np.mean(x_rr_l))
+        mean_lp.append(np.mean(x_lp_l))
+
+    err_rr = [(hi - lo) for lo, hi in ci_rr]
+    err_lp = [(hi - lo) for lo, hi in ci_lp]
+
+    return mse_rr, mse_lp, ci_rr, ci_lp, err_rr, err_lp, mean_rr, mean_lp, true_mean
 
 
 def _ci(sample, alpha=0.05):  # (Asymptotic) Confidence interval.
@@ -94,7 +114,9 @@ def _uniform(epsilon=1, t=1000, ns=(10, 100, 1000, 10000)):
     def sample_x(sample_size):
         return torch.rand(size=(sample_size,)), 0.5
 
-    mse_rr, mse_lp = _make_mse_estimates(ns=ns, t=t, epsilon=epsilon, sample_x=sample_x)
+    mse_rr, mse_lp, ci_rr, ci_lp, err_rr, err_lp, mean_rr, mean_lp, true_mean = _make_estimates(
+        ns=ns, t=t, epsilon=epsilon, sample_x=sample_x
+    )
     plots = [dict(x=ns, y=mse_rr, label="RR"), dict(x=ns, y=mse_lp, label="Laplace")]
     utils.plot_wrapper(
         img_path="./uniform_mse",
@@ -103,12 +125,25 @@ def _uniform(epsilon=1, t=1000, ns=(10, 100, 1000, 10000)):
         options=dict(ylabel="$\mathrm{Unif}[0, 1] \quad \mathrm{MSE}$", xscale="log", xlabel="$N$")
     )
 
+    errorbars = [
+        dict(x=ns, y=mean_rr, yerr=err_rr, label="RR", alpha=0.8),
+        dict(x=ns, y=mean_lp, yerr=err_lp, label="Laplace", alpha=0.8),
+    ]
+    utils.plot_wrapper(
+        img_path="./uniform_ci",
+        suffixes=('.png', '.pdf'),
+        errorbars=errorbars,
+        options=dict(ylabel="$\mathrm{Unif}[0, 1] \quad \theta$", xscale="log", xlabel="$N$")
+    )
+
 
 def _bernoulli(epsilon=1, t=1000, ns=(10, 100, 1000, 10000), pfloat=0.1):
     def sample_x(sample_size):
         return torch.bernoulli(torch.full(size=(sample_size,), fill_value=pfloat)), pfloat
 
-    mse_rr, mse_lp = _make_mse_estimates(ns=ns, t=t, epsilon=epsilon, sample_x=sample_x)
+    mse_rr, mse_lp, ci_rr, ci_lp, err_rr, err_lp, mean_rr, mean_lp, true_mean = _make_estimates(
+        ns=ns, t=t, epsilon=epsilon, sample_x=sample_x
+    )
     plots = [dict(x=ns, y=mse_rr, label="RR"), dict(x=ns, y=mse_lp, label="Laplace")]
     utils.plot_wrapper(
         img_path="./bernoulli_mse",
@@ -117,18 +152,42 @@ def _bernoulli(epsilon=1, t=1000, ns=(10, 100, 1000, 10000), pfloat=0.1):
         options=dict(ylabel="$\mathrm{Bern}(p=0.1) \quad \mathrm{MSE}$", xscale="log", xlabel="$N$")
     )
 
+    errorbars = [
+        dict(x=ns, y=mean_rr, yerr=err_rr, label="RR", alpha=0.8),
+        dict(x=ns, y=mean_lp, yerr=err_lp, label="Laplace", alpha=0.8),
+    ]
+    utils.plot_wrapper(
+        img_path="./bernoulli_ci",
+        suffixes=('.png', '.pdf'),
+        errorbars=errorbars,
+        options=dict(ylabel="$\mathrm{Bern}(p=0.1) \quad \theta$", xscale="log", xlabel="$N$")
+    )
+
 
 def _tight_uniform(epsilon=1, t=1000, ns=(10, 100, 1000, 10000)):
     def sample_x(sample_size):
         return torch.rand(size=(sample_size,)) * 0.02 + 0.49, 0.50
 
-    mse_rr, mse_lp = _make_mse_estimates(ns=ns, t=t, epsilon=epsilon, sample_x=sample_x)
+    mse_rr, mse_lp, ci_rr, ci_lp, err_rr, err_lp, mean_rr, mean_lp, true_mean = _make_estimates(
+        ns=ns, t=t, epsilon=epsilon, sample_x=sample_x
+    )
     plots = [dict(x=ns, y=mse_rr, label="RR"), dict(x=ns, y=mse_lp, label="Laplace")]
     utils.plot_wrapper(
         img_path="./tight_uniform_mse",
         suffixes=('.png', '.pdf'),
         plots=plots,
         options=dict(ylabel="$\mathrm{Unif}[0.49, 0.51] \quad \mathrm{MSE}$", xscale="log", xlabel="$N$")
+    )
+
+    errorbars = [
+        dict(x=ns, y=mean_rr, yerr=err_rr, label="RR", alpha=0.8),
+        dict(x=ns, y=mean_lp, yerr=err_lp, label="Laplace", alpha=0.8),
+    ]
+    utils.plot_wrapper(
+        img_path="./tight_uniform_ci",
+        suffixes=('.png', '.pdf'),
+        errorbars=errorbars,
+        options=dict(ylabel="$\mathrm{Unif}[0.49, 0.51] \quad \theta$", xscale="log", xlabel="$N$")
     )
 
 
