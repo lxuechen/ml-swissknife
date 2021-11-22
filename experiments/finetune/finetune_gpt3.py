8793996ed19b72fa84752f9ec97dfd112edaa67c
Xuechen Li <12689993+lxuechen@users.noreply.github.com>
2021-11-22 12:31:41 -0800

    GPT-3 fine-tuning.

--- experiments/finetune/finetune_gpt3.py
+++ experiments/finetune/finetune_gpt3.py
@@ -1,66 +1,59 @@
 """
 Helpers for fine-tuning GPT-3.
+
+To download dataset automatically, run
+    pip install gdown
 """
 
 import json
+import logging
 import os
-import sys
+import subprocess
 
 import fire
 
-from swissknife import utils
+E2E_data_url = "https://drive.google.com/uc?id=1RNO-Ciz0-WU4-nyYzukbzXvq76TIZd5j"
 
 
-# TODO: Wrap this nicely!
-# TODO: Use more epochs on a smaller dataset
-# TODO: Best model curie:ft-user-tfmcepzu9e4dn4mfwej3x788-2021-11-22-01-51-37
-def format_e2e(
-    base_dir="/Users/xuechenli/data/prefix-tuning/data/e2e_data",
-    out_dir="/Users/xuechenli/data/e2e_gpt3_full",
+def get_latest_model_id(**kwargs):
+    """Get the id of the last fine-tuned model."""
+    cmd = 'openai api fine_tunes.list'
+    result = subprocess.check_output(cmd, shell=True)
+    result = json.loads(result)
+    last_job_result = result["data"][-1]
+    print("Last job stats: ")
+    print(json.dumps(last_job_result, indent=4))
 
-    prompt_ending="\n\n###\n\n",
-    completion_ending=" END",
-    target_size=sys.maxsize,
-    files=(
-        ("src1_train.txt", "train.jsonl"),
-        ("src1_valid.txt", "valid.jsonl"),
-        ("src1_test.txt", "test.jsonl"),
-    )
-):
-    """Preprocess the e2e files into GPT-3 requested format."""
-    for in_file, out_file in files:
-        in_path = utils.join(base_dir, in_file)
-        out_path = utils.join(out_dir, out_file)
-
-        outs = []
-        for line in utils.readlines(in_path):
-            # TODO: Do the extra whitespaces affect performance?
-            prompt, completion = line.split('||')
-            outs.append(
-                dict(prompt=prompt + prompt_ending, completion=completion + completion_ending)
-            )
-            if len(outs) >= target_size:
-                break
-
-        os.makedirs(os.path.dirname(out_path), exist_ok=True)
-        with open(out_path, 'w') as f:
-            for out in outs:
-                f.write(json.dumps(out) + '\n')
+    print("\n\nLast job id: ")
+    print(last_job_result["fine_tuned_model"])
 
 
 def finetune(
-    train_path="/Users/xuechenli/data/e2e_gpt3_full/train.jsonl",
+    data_dir=os.path.join(os.path.expanduser('~'), 'data', 'e2e_gpt3_full'),
+    download=True,  # Download the data if not found.
+
     # One of ada, babbage, curie.
     # Performance comparisons: generally ada > babbage > curie.
     # line up ada (350m), babbage (1.5b), curie (6.7b), davinci (175b).
     # https://blog.eleuther.ai/gpt3-model-sizes/
     base_model="curie",
     # Setting epochs to be more than 5 gives the error:
-    # You should not set n_epochs greater than 5. Please contact us if you'd like to set n_epochs to higher than 5.
+    #   You should not set n_epochs greater than 5. Please contact us if you'd like to set n_epochs to higher than 5.
     n_epochs=5,
     learning_rate_multiplier=0.1,
 ):
-    """Fine-tune with one of the GPT-3 models."""
+    """Fine-tune with one of the GPT-3 models of some size."""
+    if not os.path.exists(data_dir):
+        if not download:
+            raise ValueError("Did not find data. Set `download` to `True` to download it automatically.")
+        logging.info("Downloading E2E data...")
+
+        data_dir_par = os.path.dirname(data_dir)
+        out_path = os.path.join(data_dir_par, 'e2e.zip')
+        os.system(f'gdown {E2E_data_url} -O {out_path}')
+        os.system(f'unzip {out_path} -d {data_dir_par}')
+
+    train_path = os.path.join(data_dir, 'train.jsonl')
     os.system(
         f'openai api fine_tunes.create '
         f'-t {train_path} '
@@ -70,20 +63,21 @@ def finetune(
     )
 
 
-def inference(prompt):
-    # TODO: Use more tokens.
+def generate(prompt, max_tokens=20):
     os.system(
-        f'openai api completions.create -m curie:ft-user-tfmcepzu9e4dn4mfwej3x788-2021-11-22-00-39-24 -p "{prompt}"'
+        f'openai api completions.create -m curie:ft-user-tfmcepzu9e4dn4mfwej3x788-2021-11-22-00-39-24 '
+        f'-p "{prompt}" '
+        f'-M {max_tokens}'
     )
 
 
-def main(task="format_e2e", **kwargs):
-    if task == "format_e2e":
-        format_e2e(**kwargs)
-    elif task == "finetune":
+def main(task="finetune", **kwargs):
+    if task == "finetune":
         finetune(**kwargs)
-    elif task == "inference":
-        inference(**kwargs)
+    elif task == "generate":
+        generate(**kwargs)
+    elif task == "get_latest_model_id":
+        get_latest_model_id(**kwargs)
 
 
 if __name__ == "__main__":
