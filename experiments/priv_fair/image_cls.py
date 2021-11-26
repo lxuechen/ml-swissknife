63e0cb0fb88be109092fdf959334e5bd53c3cae0
Xuechen Li <12689993+lxuechen@users.noreply.github.com>
2021-11-26 00:47:22 -0800

    Remove data aug.

--- experiments/priv_fair/image_cls.py
+++ experiments/priv_fair/image_cls.py
@@ -49,8 +49,6 @@ def make_loaders(
         os.makedirs(root, exist_ok=True)
 
     transform_train = transforms.Compose([
-        transforms.RandomCrop(32, padding=4),
-        transforms.RandomHorizontalFlip(),
         transforms.ToTensor(),
         transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
     ])
