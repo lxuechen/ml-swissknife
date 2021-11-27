ce0c87aeaeb1254142a1254d609a74f53dd7956f
Xuechen Li <12689993+lxuechen@users.noreply.github.com>
2021-11-27 12:13:12 -0800

    Fix scale.

--- experiments/priv_fair/plots/priv_fair_scale.py
+++ experiments/priv_fair/plots/priv_fair_scale.py
@@ -37,12 +37,45 @@ def main(
 
             errorbars.append(errorbar)
 
+    img_path = "/Users/xuechenli/remote/swissknife/experiments/priv_fair/plots/depth"
     utils.plot_wrapper(
+        img_path=img_path,
+        suffixes=(".png", ".pdf"),
         errorbars=errorbars,
         options=dict(xlabel="Depth (number of layers)", ylabel="CIFAR-10 Test error"),
     )
 
     # Show scale w.r.t. width.
+    errorbars = []
+
+    width_factors = (1, 2, 3)
+    for sk_mode in (1,):
+        for depth in (152,):
+            errorbar = dict(x=width_factors, y=[], yerr=[], label=f"depth={depth}, sk-mode={sk_mode}")
+
+            for width_factor in width_factors:
+                model = "simclr_" + f'r{depth}_{width_factor}x_sk{sk_mode}'
+
+                vals = []
+                for seed in seeds:
+                    log_history_path = os.path.join(base_dir, model, f"{seed}", "log_history.json")
+                    log_history = utils.jload(log_history_path)
+                    last_dump = log_history[-1]
+                    vals.append(last_dump['test_zeon'])
+
+                avg, std = utils.single_standard_deviation(vals)
+                errorbar["y"].append(avg)
+                errorbar["yerr"].append(std)
+
+            errorbars.append(errorbar)
+
+    img_path = "/Users/xuechenli/remote/swissknife/experiments/priv_fair/plots/width"
+    utils.plot_wrapper(
+        img_path=img_path,
+        suffixes=(".png", ".pdf"),
+        errorbars=errorbars,
+        options=dict(xlabel="Width factor", ylabel="CIFAR-10 Test error"),
+    )
 
 
 if __name__ == "__main__":
