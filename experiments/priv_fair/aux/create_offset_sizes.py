ec2993b696f5051d62ab9a3fd2cabae1d1c7eaea
Xuechen Li <12689993+lxuechen@users.noreply.github.com>
2021-11-27 17:47:10 -0800

    Add imbalance.

---
+++ experiments/priv_fair/aux/create_offset_sizes.py
@@ -0,0 +1,10 @@
+"""Create json files for offsets."""
+from swissknife import utils
+
+# Only give the majority some advantage.
+for size in (10, 20, 50, 100, 200, 500, 1000, 2000,):
+    size_str = utils.int2str(size)
+    path = utils.join(".", f'offset_sizes_{size_str}.json')
+    utils.jdump(
+        {0: size}, path
+    )
