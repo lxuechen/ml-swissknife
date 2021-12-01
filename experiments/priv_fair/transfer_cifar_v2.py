637f157f63f1c6eb563d63dd7d910a9074a2338e
Xuechen Li <12689993+lxuechen@users.noreply.github.com>
2021-12-01 13:32:46 -0800

    Fix message for non-private.

--- experiments/priv_fair/transfer_cifar_v2.py
+++ experiments/priv_fair/transfer_cifar_v2.py
@@ -182,7 +182,7 @@ def non_private_training(
         )
         if len(ood_datasets) > 0 and len(ood_loaders) > 0:
             for ood_dataset, ood_loader in utils.zip_(ood_datasets, ood_loaders):
-                ood_xent, ood_zeon = test(model, ood_loader)
+                ood_xent, ood_zeon = test(model, ood_loader, msg=f"{ood_dataset}: ")
                 results[ood_dataset] = dict(test_xent=ood_xent, test_zeon=ood_zeon)
 
         history.append(results)
