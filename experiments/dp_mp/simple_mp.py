a8e4952c6f13f2464395717956daf11da7226f9a
Xuechen Li <12689993+lxuechen@users.noreply.github.com>
2021-12-25 10:49:24 -0800

    Test doesn't match up.

--- experiments/dp_mp/simple_mp.py
+++ experiments/dp_mp/simple_mp.py
@@ -1,8 +1,4 @@
-"""Simple model parallelism demonstration.
-
-TODO: Get DP-SGD running on mock data.
-TODO: Test against single GPU updates.
-"""
+"""Simple model parallelism demonstration."""
 
 import fire
 import private_transformers
@@ -22,17 +18,35 @@ def get_base_device(base_device_index=0):
     return get_device_info(base_device_index=base_device_index)[2]
 
 
-def get_batch(batch_size=16, seq_len=100):
-    """Create a mock batch of data."""
-    x = torch.randint(size=(batch_size, seq_len), low=1, high=100)
+def get_batch(batch_size=4, seq_len=100, disable_randomness=True):
+    """Create a mock batch of data.
+
+    Always generate the same batch when `disable_randomness` is True.
+    """
+    generator = torch.Generator()
+    if disable_randomness:
+        generator.manual_seed(0)
+    x = torch.randint(size=(batch_size, seq_len), low=1, high=100, generator=generator)
     x = x.to(get_base_device())
     return x
 
 
-def main(batch_size=16):
-    model: transformers.GPT2LMHeadModel = transformers.GPT2LMHeadModel.from_pretrained('gpt2')
-    model.parallelize()
-    optimizer = torch.optim.Adam(params=model.parameters())
+def train_gpt2_with_dp_on_mock_data(
+    model=None, mp=True, base_device=0, batch_size=4, num_updates=3,
+    disable_randomness=True,
+):
+    if model is None:
+        model: transformers.GPT2LMHeadModel = transformers.GPT2LMHeadModel.from_pretrained('gpt2')
+    if disable_randomness:
+        model.config.attn_pdrop = 0.
+        model.config.resid_pdrop = 0.
+        model.config.embd_pdrop = 0.
+
+    if mp:
+        model.parallelize()  # This essentially does everything for model parallelism.
+    else:
+        model.to(base_device)
+    optimizer = torch.optim.Adam(params=model.parameters(), lr=1e-3)
 
     privacy_engine = private_transformers.PrivacyEngine(
         module=model,
@@ -44,11 +58,10 @@ def main(batch_size=16):
         epochs=3,
     )
     privacy_engine.attach(optimizer=optimizer)
-    print(privacy_engine)
 
     model.train()
-    for _ in range(3):
-        input_ids = get_batch()
+    for _ in range(num_updates):
+        input_ids = get_batch(batch_size=batch_size, disable_randomness=disable_randomness)
         outputs = model(input_ids, return_dict=True)
         labels = input_ids[:, 1:, ]
         logits = outputs.logits[:, :-1, :].permute(0, 2, 1)
@@ -57,6 +70,27 @@ def main(batch_size=16):
         # This step is different from existing workflows:
         #   Don't call `loss.backward`; leave it to `optimizer.step` to handle backward.
         optimizer.step(loss=loss)
+        del input_ids, outputs, labels, logits
+
+    model.cpu()
+    del optimizer, privacy_engine
+    torch.cuda.empty_cache()
+    return model
+
+
+def main():
+    torch.set_default_dtype(torch.float64)
+
+    mp_final_model = train_gpt2_with_dp_on_mock_data(mp=True, disable_randomness=True)
+    final_model = train_gpt2_with_dp_on_mock_data(mp=False, disable_randomness=True)
+
+    mp_final_model_flat = torch.cat(
+        tuple(t.flatten() for t in mp_final_model.parameters())
+    )
+    final_model_flat = torch.cat(
+        tuple(t.flatten() for t in final_model.parameters())
+    )
+    print(torch.norm(mp_final_model_flat - final_model_flat))
 
 
 if __name__ == "__main__":
