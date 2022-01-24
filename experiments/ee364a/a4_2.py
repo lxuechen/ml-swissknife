79fcabe27486f87750aea310694781839fe310ee
Xuechen Li <12689993+lxuechen@users.noreply.github.com>
2022-01-24 10:26:09 -0800

    Check in problem.

--- experiments/ee364a/a4_2.py
+++ experiments/ee364a/a4_2.py
@@ -3,49 +3,24 @@ import fire
 import numpy as np
 
 
-def obj_a(x):
-    return cp.Minimize(cp.sum(x * np.array([1., 1.])))
-
-
-def obj_b(x):
-    return cp.Minimize(cp.sum(x * np.array([-1., -1.])))
-
-
-def obj_c(x):
-    return cp.Minimize(cp.sum(x * np.array([1., 0.])))
-
-
-def obj_d(x):
-    return cp.Minimize(cp.max(x))
-
-
-def obj_e(x):
-    return cp.Minimize(x[0] ** 2 + 9. * x[1] ** 2.)
-
-
 def main():
-    d = 2
-
     for obj_func in (
-        obj_a,
-        obj_b,
-        obj_c,
-        obj_d,
-        obj_e,
+        lambda t: cp.Minimize(cp.sum(t * np.array([1., 1.]))),
+        lambda t: cp.Minimize(cp.sum(t * np.array([-1., -1.]))),
+        lambda t: cp.Minimize(cp.sum(t * np.array([1., 0.]))),
+        lambda t: cp.Minimize(cp.max(t)),
+        lambda t: cp.Minimize(t[0] ** 2 + 9. * t[1] ** 2.),
     ):
-        x = cp.Variable(d)
+        x = cp.Variable(2)
         objective = obj_func(x)
-
         constraints = [
             2 * x[0] + x[1] >= 1,
             x[0] + 3 * x[1] >= 1,
             x >= np.zeros_like(x),
         ]
         prob = cp.Problem(objective, constraints)
-
         result = prob.solve()
-        print(result)
-        print(x.value)
+        print(result, x.value)
 
 
 if __name__ == "__main__":
