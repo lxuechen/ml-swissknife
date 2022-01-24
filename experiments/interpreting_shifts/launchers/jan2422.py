3c0f30aaeb9576c48bb6e9c92492a329f1104dd9
Xuechen Li <12689993+lxuechen@users.noreply.github.com>
2022-01-24 14:24:18 -0800

    Reduce jobs.

--- experiments/interpreting_shifts/launchers/jan2422.py
+++ experiments/interpreting_shifts/launchers/jan2422.py
@@ -103,7 +103,7 @@ def main(
             for train_epochs in (0,):
                 for match_epochs in (10,):
                     for feature_extractor in ('id',):
-                        for reg_target in (0.01, 0.05, 0.1, 1):
+                        for reg_target in (0.01, 0.1, 1):
                             for reg_entropy in (0.1, 0.5, 1):
                                 for eval_batch_size in (250, 500, 1000,):
                                     commands.append(
