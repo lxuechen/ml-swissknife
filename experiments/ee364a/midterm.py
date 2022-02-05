f30890421e2a667517abec13c8aa9e03c283ce58
Xuechen Li <12689993+lxuechen@users.noreply.github.com>
2022-02-04 22:55:11 -0800

    Check in.

---
+++ experiments/ee364a/midterm.py
@@ -0,0 +1,18 @@
+import cvxpy as cp
+
+# x = cp.Variable(shape=(1,))
+# y = cp.Variable(shape=(1,))
+# z = cp.Variable(shape=(1,))
+# objective = cp.Maximize(
+#     cp.sqrt(1 + 4 * cp.square(x) + 16 * cp.square(y))
+# )
+# objective = cp.Maximize(
+#     cp.minimum(x, cp.log(y)) - cp.maximum(y, z)
+# )
+# objective = cp.Minimize(
+#     cp.log(
+#         cp.exp(2 * x + 3) + cp.exp(4 * y + 5)
+#     )
+# )
+# res = objective.is_dcp(dpp=False)
+# print(res)
