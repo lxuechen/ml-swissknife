bfbdab7c7a5f82bd994e1d004f8f0ef9e09db92c
Xuechen Li <12689993+lxuechen@users.noreply.github.com>
2022-02-04 23:13:48 -0800

    Add plotting.

--- experiments/ee364a/a16_2.py
+++ experiments/ee364a/a16_2.py
@@ -13,6 +13,32 @@ gamma = 1.
 K = 35
 
 
+def plot(p, f):
+    import matplotlib.pyplot as plt
+    from mpl_toolkits.mplot3d import Axes3D
+    from matplotlib import cm
+
+    fig = plt.figure()
+    ax = fig.gca(projection="3d")
+    X = np.linspace(-40, 55, num=30)
+    Y = np.linspace(0, 55, num=30)
+    X, Y = np.meshgrid(X, Y)
+    Z = alpha * np.sqrt(X ** 2 + Y ** 2)
+    ax.plot_surface(X, Y, Z, rstride=1, cstride=1, cmap=cm.autumn, linewidth=0.1, alpha=0.7, edgecolors="k")
+    ax = plt.gca();
+    ax.view_init(azim=225)
+
+    # Have your solution be stored in p
+    ax.plot(xs=p.value[0, :], ys=p.value[1, :], zs=p.value[2, :], c='b', lw=2, zorder=5)
+    ax.quiver(p.value[0, :-1], p.value[1, :-1], p.value[2, :-1],
+              f.value[0, :], f.value[1, :], f.value[2, :], zorder=5, color="black")
+
+    ax.set_xlabel("x");
+    ax.set_ylabel("y");
+    ax.set_zlabel("z")
+    plt.show()
+
+
 def main():
     f = cp.Variable(shape=(K + 1, 3))
     v = cp.Variable(shape=(K + 2, 3))
@@ -50,6 +76,8 @@ def main():
     prob = cp.Problem(objective, constraints)
     result = prob.solve()
     print(result)
+
+    plot(p=cp.transpose(p), f=cp.transpose(f))
 
 
 if __name__ == "__main__":
