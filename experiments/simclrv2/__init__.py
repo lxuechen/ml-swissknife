4882d2ee3f0346793e87e512fd8db0cb57cde36c
Xuechen Li <12689993+lxuechen@users.noreply.github.com>
2022-02-10 14:06:50 -0800

    Add example commands.

--- experiments/simclrv2/__init__.py
+++ experiments/simclrv2/__init__.py
@@ -5,4 +5,8 @@ Convert tf models to pytorch, based on the codebase
 Download -> convert -> verify.
 
 Needs Python 3.7 to run tf==1.15.4
+
+python download.py r152_3x_sk1
+python convert.py r152_3x_sk1/model.ckpt-250228 --ema
+python verify.py r152_3x_sk1.pth
 """
