ab36e28252b7268d211973a0d70de7d4955c5209
Xuechen Li <12689993+lxuechen@users.noreply.github.com>
2022-02-10 14:01:08 -0800

    Fix conversion.

---
+++ experiments/simclrv2/__init__.py
@@ -0,0 +1,8 @@
+"""
+Convert tf models to pytorch, based on the codebase
+    https://github.com/Separius/SimCLRv2-Pytorch
+
+Download -> convert -> verify.
+
+Needs Python 3.7 to run tf==1.15.4
+"""
