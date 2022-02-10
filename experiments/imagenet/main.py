0cdf03b77ed3ec78fe1fb4a8ad6e333048b378d5
Xuechen Li <12689993+lxuechen@users.noreply.github.com>
2022-02-09 23:23:30 -0800

    Process dogs dataset.

---
+++ experiments/imagenet/main.py
@@ -0,0 +1,41 @@
+"""Preprocess imagenet.
+
+Create the dogs dataset.
+
+Run command in the imagenet folder:
+    python main.py
+"""
+import os
+
+import fire
+import tqdm
+
+from swissknife import utils
+
+DOG_CLASS_IDS = tuple(range(151, 269))
+IMAGENET_SOURCE_DIR = "/u/scr/nlp/data/imagenet"
+
+
+def main(
+    target_dir="/u/scr/nlp/data/lxuechen-data/imagenet-dogs",
+):
+    class_info_path = os.path.join('.', 'wordnet', 'dataset_class_info.json')
+    class_info = utils.jload(class_info_path)
+
+    dog_classes = []
+    for class_id, folder_id, class_label in class_info:
+        if class_id in DOG_CLASS_IDS:
+            dog_classes.append((class_id, folder_id, class_label))
+
+    for split in tqdm.tqdm(('train', 'val'), desc="split"):
+        for dog_class in tqdm.tqdm(dog_classes, desc="classes"):
+            dog_class_folder = dog_class[1]
+            this_source_dir = utils.join(IMAGENET_SOURCE_DIR, split, dog_class_folder)
+            this_target_dir = utils.join(target_dir, split)
+
+            os.system(f'mkdir -p {target_dir}')
+            os.system(f'cp -r {this_source_dir} {this_target_dir}')
+
+
+if __name__ == "__main__":
+    fire.Fire(main)
---
+++ experiments/imagenet/wordnet/README.md
@@ -0,0 +1,1 @@
+Folder copied from [Shibani's BREEDS codebase](https://github.com/MadryLab/BREEDS-Benchmarks/tree/master/imagenet_class_hierarchy/wordnet).
