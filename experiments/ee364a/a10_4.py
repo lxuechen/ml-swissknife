3beb43c47d3c62a8e5045f654b3d2e395b3ea445
Xuechen Li <12689993+lxuechen@users.noreply.github.com>
2022-03-08 15:23:02 -0800

    Fix template.

--- experiments/ee364a/a10_4.py
+++ experiments/ee364a/a10_4.py
@@ -13,6 +13,7 @@ the residual at the current iterate
 
 Common mistakes:
     - nan due to division
+    - not checking if iterate still in domain
 """
 
 from dataclasses import dataclass
@@ -20,13 +21,6 @@ from typing import Optional
 
 import fire
 import torch
-
-
-@dataclass
-class LPCenteringProb:
-    A: torch.Tensor
-    b: torch.Tensor
-    c: torch.Tensor
 
 
 @dataclass
@@ -42,6 +36,16 @@ class Soln:
 
     def norm(self):
         return torch.cat([self.x.flatten(), self.nu.flatten()]).norm(p=2)
+
+
+@dataclass
+class LPCenteringProb:
+    A: torch.Tensor
+    b: torch.Tensor
+    c: torch.Tensor
+
+    def loss(self, soln: Soln):
+        return self.c @ soln.x - torch.log(soln.x).sum()
 
 
 def _solve_residual(soln: Soln, prob: LPCenteringProb):
@@ -71,7 +75,7 @@ def _solve_kkt(soln: Soln, prob: LPCenteringProb):
     return Soln(x=v, nu=w)  # Descent direction.
 
 
-def solve(soln: Soln, prob: LPCenteringProb, alpha, beta, max_steps=50, epsilon=10 ** -6, return_residual_norms=False):
+def solve(soln: Soln, prob: LPCenteringProb, alpha, beta, max_steps=50, epsilon=10 ** -6):
     if not torch.all(soln.x > 0):
         raise ValueError("Initial iterate not in domain")
     if not (0 < alpha < .5) or not (0 < beta < 1.):
@@ -79,10 +83,14 @@ def solve(soln: Soln, prob: LPCenteringProb, alpha, beta, max_steps=50, epsilon=
 
     this_step = 0
     residual_norms = []
+    losses = []
     steps = []
     while True:
         res = _solve_residual(soln=soln, prob=prob)
         direction = _solve_kkt(soln, prob)
+        print(res)
+        print(direction)
+        import pdb; pdb.set_trace()
 
         steps.append(this_step)
         residual_norms.append(res.norm().item())
@@ -104,13 +112,12 @@ def solve(soln: Soln, prob: LPCenteringProb, alpha, beta, max_steps=50, epsilon=
         if this_step >= max_steps or res.norm() <= epsilon:
             break
 
-    if return_residual_norms:
-        return soln, steps, residual_norms
-    else:
-        return soln, steps
+    return soln, steps, residual_norms, losses
 
 
-def _generate_prob(m=5, n=10):
+def _generate_prob():
+    m = 5
+    n = 10
     A = torch.randn(m, n)
     A[0].abs_()
     rank = torch.linalg.matrix_rank(A)
@@ -118,11 +125,46 @@ def _generate_prob(m=5, n=10):
 
     p = torch.randn(n).abs()  # Make positive.
     b = A @ p
-    c = torch.randn(n).exp()
+    c = torch.zeros(n)
 
     x = torch.randn(n).exp() * 2  # Make positive.
     nu = torch.randn(m)
     return Soln(x=x, nu=nu), LPCenteringProb(A=A, b=b, c=c)
+
+
+def _load_example_prob():
+    import numpy as np
+    m = 4
+    n = 8
+    np.random.seed(364)
+    A = np.round(np.random.rand(m, n), 2)
+    A = np.vstack((A, np.ones(n)));
+    p = np.random.rand(n);
+    b = np.round(A.dot(p), 2)
+    c = np.round(np.random.rand(n), 2)
+    x0 = np.random.rand(n)
+    nu = np.zeros((m + 1,))
+    A, b, c, x0,nu = tuple(
+        torch.from_numpy(a) for a in (A, b, c, x0, nu)
+    )
+    return Soln(x=x0, nu=nu), LPCenteringProb(A=A, b=b, c=c)
+
+    # Use alpha = 0.1
+    #     beta = 0.5
+    #     epsilon = 1e-5
+
+    # RESULTS OF FIRST ITERATION
+    # dual_residual   = np.array([-9.45445041e-01, -8.68890819e-01, -6.19225322e-01, -1.36394692e-01,
+    # -2.80209340e-01, -2.29080275e+02, -1.74922026e+01, -9.77238004e-01,])
+    # primal_residual = np.array([0.76155693, 1.26350154, 1.59255689, 0.74703098, 2.0962205,])
+    # residuals       = 229.774887
+    # direction_x     = np.array([-0.20992729,  0.01027243, -0.52409603, -0.33186397, -0.62116449,  0.00432129,
+    # 0.05484001, -0.47860245])
+    # direction_nu    = np.array([ 1.46632973, -1.64664555,  2.18458155,  1.37453686, -0.23884889,])
+    # DO NOT USE == TO COMPARE - CHECK norm(your_value, our_value) IS SMALL
+
+    # LINE SEARCH HINT: If the step length is too long, the tentative point can leave the domain.
+    # Make sure to check your point is in the domain before testing whether you should terminate the line search.
 
 
 @torch.no_grad()
@@ -130,14 +172,18 @@ def main(seed=0):
     torch.manual_seed(seed)
     torch.set_default_dtype(torch.float64)
 
-    soln, prob = _generate_prob()
-    soln, steps, residual_norms = solve(
+    # soln, prob = _generate_prob()
+
+    soln, prob = _load_example_prob()
+
+    soln, steps, residual_norms, losses = solve(
         soln=soln, prob=prob,
-        alpha=0.3, beta=0.9, return_residual_norms=True, max_steps=100,
+        alpha=0.1, beta=0.5, epsilon=1e-5,
     )
-    print(soln)
-    print(steps)
-    print(residual_norms)
+    # print(soln)
+    # print(steps)
+    # print(residual_norms)
+    # print(losses)
 
 
 if __name__ == "__main__":
