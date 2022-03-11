83fa8f13adfaac768e54d90e003df168ed2bee52
Xuechen Li <12689993+lxuechen@users.noreply.github.com>
2022-03-11 12:02:21 -0800

    Fixes.

--- experiments/explainx/launchers/analyze.py
+++ experiments/explainx/launchers/analyze.py
@@ -7,12 +7,12 @@ import os
 import fire
 
 
-def main():
-    command = '''python -m explainx.loop \
+def main(target="blond hair"):
+    command = f'''python -m explainx.loop \
         --task analyze \
         --train_dir /nlp/scr/lxuechen/explainx/mar1022/linear_probe_True_model_name_openai_clip-vit-base-patch32 \
-        --ckpt_file global_step_9800.000000.ckpt \
-        --num_per_group 200
+        --num_per_group 200 \
+        --target "{target}"
     '''
     os.system(command)
 
