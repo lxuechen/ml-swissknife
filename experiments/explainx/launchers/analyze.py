b72fd8325be6a3639c0c914c16e49289b35f1eea
Xuechen Li <12689993+lxuechen@users.noreply.github.com>
2022-03-11 12:08:39 -0800

    Fix bug.

--- experiments/explainx/launchers/analyze.py
+++ experiments/explainx/launchers/analyze.py
@@ -2,19 +2,29 @@
 python -m explainx.launchers.analyze
 """
 
-import os
-
 import fire
 
 
 def main(target="blond hair"):
+    commands = []
     command = f'''python -m explainx.loop \
         --task analyze \
         --train_dir /nlp/scr/lxuechen/explainx/mar1022/linear_probe_True_model_name_openai_clip-vit-base-patch32 \
         --num_per_group 200 \
         --target "{target}"
     '''
-    os.system(command)
+    commands.append(command)
+
+    command = f'''python -m explainx.loop \
+        --task analyze \
+        --train_dir /nlp/scr/lxuechen/explainx/mar1122/linear_probe_True_model_name_openai_clip-vit-base-patch32 \
+        --num_per_group 200 \
+        --target "black hair"
+    '''
+    commands.append(command)
+
+    from swissknife import utils
+    utils.gpu_scheduler(commands, log=False)
 
 
 if __name__ == "__main__":
