ba5db91aa8118e2fe338fcd3f106528af04fb532
Xuechen Li <12689993+lxuechen@users.noreply.github.com>
2022-03-23 14:42:28 -0700

    Fix check len.

--- experiments/nlpdata/imdb.py
+++ experiments/nlpdata/imdb.py
@@ -7,22 +7,28 @@ import numpy as np
 import transformers
 
 
-def main():
-    data = datasets.load_dataset('yelp_review_full')
-    print(data)
-
-    data = datasets.load_dataset('imdb')
-    tokenizer = transformers.AutoTokenizer.from_pretrained('roberta-base')
+def _check_len(texts, tokenizer, msg=""):
     lens = []
-    for text in data['train']['text']:
+    for text in texts:
         lens.append(len(tokenizer.encode(text)))
 
     print(
+        f'msg: {msg}, '
         f'mean: {np.mean(lens)}, '
         f'median: {np.median(lens)}, '
         f'max: {np.max(lens)}, '
         f'min: {np.min(lens)}'
     )
+
+
+def main():
+    tokenizer = transformers.AutoTokenizer.from_pretrained('roberta-base')
+
+    data = datasets.load_dataset('yelp_review_full')
+    _check_len(data['train']['text'], tokenizer)
+
+    data = datasets.load_dataset('imdb')
+    _check_len(data['train']['text'], tokenizer)
 
 
 if __name__ == "__main__":
