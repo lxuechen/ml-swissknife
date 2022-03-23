89ed29d3b9e6948b0a7ed9bd2c950b91c59dd997
Xuechen Li <12689993+lxuechen@users.noreply.github.com>
2022-03-23 10:16:10 -0700

    Wrap code.

--- experiments/explainx/common.py
+++ experiments/explainx/common.py
@@ -1,11 +1,12 @@
-import torch
+import os
 
-from swissknife import utils
+from .BLIP.models import blip
 
-if torch.cuda.is_available():
-    root = "/home/lxuechen_stanford_edu/data"
-else:
-    root = "/Users/xuechenli/data"
 
-celeba_data_path = utils.join(root, "img_align_celeba")
-metadata_path = utils.join(root, "list_attr_celeba.txt")
+def make_image2text_model(image_size, beam_search_mode="contrastive", vit="base"):
+    model_url = 'https://storage.googleapis.com/sfr-vision-language-research/BLIP/models/model*_base_caption.pth'
+    med_config = os.path.join('.', 'explainx', 'BLIP', 'configs', 'med_config.json')
+    return blip.blip_decoder(
+        pretrained=model_url, image_size=image_size, vit=vit, med_config=med_config,
+        beam_search_mode=beam_search_mode,  # Most important thing!
+    )
