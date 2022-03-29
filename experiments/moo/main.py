6c3d96b85ce573e55343ed98e760d18beda4ec15
Xuechen Li <12689993+lxuechen@users.noreply.github.com>
2022-03-28 23:08:35 -0400

    Enable mean shift.

--- experiments/moo/main.py
+++ experiments/moo/main.py
@@ -12,16 +12,20 @@ from swissknife import utils
 
 
 def make_data(n_train, n_test, d, obs_noise_std=1):
-    beta = torch.randn(d, 1).exp() * torch.randn(d, 1).sign()
+    beta = torch.randn(d, 1).abs() * torch.randn(d, 1).sign()
+
+    # TODO: Is mean shift really a problem?
+    mu1 = torch.full(size=(d,), fill_value=-0.5)
+    mu2 = torch.full(size=(d,), fill_value=0.5)
     std1 = torch.cat([torch.randn(d // 2) * .3, torch.randn(d // 2) * 3.])
     std2 = torch.cat([torch.randn(d // 2) * 3., torch.randn(d // 2) * .3])
-    x1_train = torch.randn(n_train // 2, d) * std1[None, :]
-    x2_train = torch.randn(n_train // 2, d) * std2[None, :]
+    x1_train = torch.randn(n_train // 2, d) * std1[None, :] + mu1[None, :]
+    x2_train = torch.randn(n_train // 2, d) * std2[None, :] + mu2[None, :]
     y1_train = x1_train @ beta
     y2_train = x2_train @ beta
 
-    x1_test = torch.randn(n_test // 2, d) * std1[None, :]
-    x2_test = torch.randn(n_test // 2, d) * std2[None, :]
+    x1_test = torch.randn(n_test // 2, d) * std1[None, :] + mu1[None, :]
+    x2_test = torch.randn(n_test // 2, d) * std2[None, :] + mu2[None, :]
     y1_test = x1_test @ beta
     y2_test = x2_test @ beta
 
@@ -44,7 +48,7 @@ def train_and_eval(
         loss = (torch.stack([loss1, loss2]) * eta).sum()
         loss.backward()
         optimizer.step()
-        print(loss1, loss2, loss, eta)
+        print(loss1, loss2, loss, eta)  # Sanity check loss stabilizes.
 
     with torch.no_grad():
         loss1 = ((model(x1_test) - y1_test) ** 2.).sum(dim=1).mean(dim=0)
@@ -75,15 +79,15 @@ def first_order():
     pass
 
 
-def main(n_train=40, n_test=300, d=10, lr=1e-1, train_steps=5000):
-    utils.manual_seed(42)
+def main(n_train=40, n_test=300, d=20, lr=1e-2, train_steps=10000):
+    utils.manual_seed(62)
 
     (x1_train, y1_train, x2_train, y2_train,
      x1_test, y1_test, x2_test, y2_test) = make_data(n_train=n_train, n_test=n_test, d=d)
 
     losses1, losses2 = brute_force(
         x1_train, y1_train, x2_train, y2_train, x1_test, y1_test, x2_test, y2_test,
-        etas=np.linspace(0.4, 0.6, num=11),
+        etas=np.linspace(0.2, 0.8, num=31),
         lr=lr, train_steps=train_steps,
     )
     plots = [dict(x=losses1, y=losses2, marker='x')]
