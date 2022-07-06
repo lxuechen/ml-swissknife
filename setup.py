043037db3bcfb9ab317e1c1f3d116bc4db13c848
Xuechen Li <lxuechen@gmail.com>
2022-07-05 23:18:08 -0700

    Finalize first version.

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 import setuptools
 
-version = "0.0.1"
+version = "0.1.0"
 
 extras_require = {
     "latex": ("bibtexparser",)
@@ -8,12 +8,13 @@ extras_require = {
 
 setuptools.setup(
     name="swissknife",
+    packages=setuptools.find_packages(exclude=['experiments', 'templates', 'latex', 'tests']),
     version=version,
+    license="MIT",
+    description="Reusable ML research primitives for fast prototyping.",
     author="Xuechen Li",
     author_email="lxuechen@cs.toronto.edu",
-    description="Reusable ML research primitives for fast prototyping. ",
     url="https://github.com/lxuechen/swissknife",
-    packages=setuptools.find_packages(exclude=['experiments']),
     install_requires=[
         'torch', 'torchvision', 'spacy', 'tqdm', 'numpy', 'scipy', 'gputil', 'fire', 'requests', 'nltk', 'transformers',
         'datasets', 'gdown>=4.4.0', 'pandas', 'pytest', 'matplotlib', 'seaborn', 'cvxpy'
