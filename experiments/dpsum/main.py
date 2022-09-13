2562e6ec3073542c343885666f9596140913ce65
Xuechen Li <lxuechen@gmail.com>
2022-09-13 13:35:05 -0700

    Simplify decoding.

--- experiments/dpsum/main.py
+++ experiments/dpsum/main.py
@@ -1,8 +1,9 @@
+import copy
 import json
 import logging
 import sys
 from dataclasses import dataclass
-from typing import Tuple, List, Union, Dict, Optional
+from typing import List, Union, Dict, Optional
 
 import fire
 import private_transformers
@@ -17,17 +18,15 @@ from torch.utils.data import Dataset, DataLoader
 import wandb
 from ml_swissknife import utils
 
-ignore_index = -100  # In `F.cross_entropy`.
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+IGNORE_INDEX = -100  # In `F.cross_entropy`.
+BAD_WORDS_IDS = [[628], [198]]
+PAD_TOKEN = "[PAD]"
 
 
 class SamsumDataset(Dataset):
-    def __init__(
-        self,
-        tokenizer: transformers.PreTrainedTokenizer,
-        max_size: int,
-        path="/home/t-lc/software_v2/GPT3/nlg_datasets/instruct_samsum/train.jsonl",
-    ):
+    def __init__(self, tokenizer: transformers.PreTrainedTokenizer, max_size: int, path: str):
         super(SamsumDataset, self).__init__()
 
         with open(path, 'r') as f:
@@ -51,18 +50,59 @@ class SamsumDataset(Dataset):
                 continue
 
             this_input_ids = source + target
-            this_labels = [ignore_index] * len(source) + target
+            this_labels = [IGNORE_INDEX] * len(source) + target
 
             input_ids.append(torch.tensor(this_input_ids, dtype=torch.long))
             labels.append(torch.tensor(this_labels, dtype=torch.long))
-        logging.warning(f'filtered {filtered_count} examples due to exceeding context window size.')
-        logging.warning(f'sample size after filtering: {len(input_ids)}')
 
         self.input_ids = input_ids
         self.labels = labels
 
-    def __getitem__(self, item: int) -> Tuple[torch.Tensor, ...]:
-        return self.input_ids[item], self.labels[item]
+        logging.warning(f'filtered {filtered_count} examples due to exceeding context window size.')
+        logging.warning(f'sample size after filtering: {len(input_ids)}')
+
+    def __getitem__(self, item: int):
+        return {"input_ids": self.input_ids[item], "labels": self.labels[item]}
+
+    def __len__(self):
+        return len(self.input_ids)
+
+
+class SamsumPromptDataset(Dataset):  # Left pad prompt dataset.
+    def __init__(self, tokenizer: transformers.PreTrainedTokenizer, max_size: int, path: str):
+        super(SamsumPromptDataset, self).__init__()
+        with open(path, 'r') as f:
+            lines = f.readlines()
+
+        tokenizer = copy.deepcopy(tokenizer)  # Avoid tampering with original tokenizer.
+        tokenizer.padding_side = "left"
+
+        filtered_count = 0
+        contexts = []
+        for index, line in tqdm.tqdm(enumerate(lines), desc="load lines", total=min(max_size, len(lines))):
+            if index >= max_size:
+                break
+
+            packet = json.loads(line.strip())
+            packet["completion"] = packet["completion"].replace('\n\n', ' ')  # Remove '\n\n'.
+
+            source = tokenizer(packet["context"])["input_ids"]
+            target = tokenizer(packet["completion"])["input_ids"]
+
+            if len(source) + len(target) > tokenizer.model_max_length:
+                filtered_count += 1
+                continue
+            contexts.append(packet["context"])
+
+        tokenized = tokenizer(contexts, padding=True, return_tensors="pt")
+        self.input_ids = tokenized["input_ids"]
+        self.attention_mask = tokenized["attention_mask"]
+
+        logging.warning(f'filtered {filtered_count} examples due to exceeding context window size.')
+        logging.warning(f'sample size after filtering: {len(self.input_ids)}')
+
+    def __getitem__(self, item: int):
+        return {"input_ids": self.input_ids[item], "attention_mask": self.attention_mask[item]}
 
     def __len__(self):
         return len(self.input_ids)
@@ -72,13 +112,11 @@ class SamsumDataset(Dataset):
 class DataCollatorForData2TextLanguageModeling:
     tokenizer: transformers.PreTrainedTokenizer
 
-    def __call__(
-        self, examples: List[Union[List[int], torch.Tensor, Dict[str, torch.Tensor]]]
-    ) -> Dict[str, torch.Tensor]:
-        input_ids, labels = zip(*examples)
-        input_ids = self._tensorize_batch(input_ids)
-        labels = self._tensorize_batch(labels)
-        return {"input_ids": input_ids, "labels": labels}
+    def __call__(self, examples: List[Dict[str, torch.Tensor]]) -> Dict[str, torch.Tensor]:
+        zipped_examples = {
+            key: [example[key] for example in examples] for key in examples[0]
+        }
+        return {key: self._tensorize_batch(value) for key, value in zipped_examples.items()}
 
     def _tensorize_batch(
         self, examples: List[Union[List[int], torch.Tensor, Dict[str, torch.Tensor]]]
@@ -97,7 +135,7 @@ class DataCollatorForData2TextLanguageModeling:
 
 @torch.enable_grad()
 def train(
-    model, optimizer, lr_scheduler, train_loader, eval_loader,
+    model, tokenizer, optimizer, lr_scheduler, train_loader, eval_loader, prompt_loader,
     epochs: int, gradient_accumulation_steps: int, non_private: bool,
 ):
     for epoch in tqdm.tqdm(range(epochs), desc="epochs"):
@@ -121,6 +159,8 @@ def train(
                 else:
                     optimizer.virtual_step(loss=loss)
 
+            decode(model, tokenizer, prompt_loader)
+
         eval_loss = inference(model, eval_loader)
         logging.warning(f"epoch: {epoch}, eval_loss: {eval_loss:.4f}, lr: {utils.get_lr(optimizer)}")
         wandb.log({"eval_loss": eval_loss})
@@ -135,7 +175,7 @@ def compute_loss(model, batch) -> torch.Tensor:  # 1-D tensor.
     shift_logits = logits[..., :-1, :]
     shift_labels = labels[..., 1:]
     seq_lens = (shift_labels != -100).sum(dim=1)
-    loss = F.cross_entropy(shift_logits.permute(0, 2, 1), shift_labels, reduction="none", ignore_index=ignore_index)
+    loss = F.cross_entropy(shift_logits.permute(0, 2, 1), shift_labels, reduction="none", ignore_index=IGNORE_INDEX)
     loss = loss.sum(dim=1) / seq_lens  # Per token loss.
     return loss
 
@@ -152,10 +192,41 @@ def inference(model, loader, max_eval_batches=sys.maxsize):
     return sum(losses) / len(losses)
 
 
+@torch.inference_mode()
+def decode(model, tokenizer, loader, max_eval_batches=sys.maxsize, max_length=200, num_beams=5):
+    references, full_generations, generations = [], [], []
+
+    model.eval()
+    for i, batch in enumerate(loader):
+        if i >= max_eval_batches:
+            break
+        input_ids = batch["input_ids"].to(device)
+        attention_mask = batch["attention_mask"].to(device)
+        output_ids = model.generate(
+            input_ids=input_ids,
+            attention_mask=attention_mask,
+            max_length=max_length + input_ids.size(dim=1),  # max_length is a misnomer.
+            do_sample=False,
+            bad_words_ids=BAD_WORDS_IDS + [[tokenizer.pad_token_id]],
+            num_return_sequences=1,
+            num_beams=num_beams,
+            pad_token_id=tokenizer.pad_token_id,
+        )
+        completion_ids = output_ids[:, input_ids.size(dim=1):]
+
+        for target_list, tokens in utils.zip_(
+            (references, generations, full_generations), (input_ids, completion_ids, output_ids)
+        ):
+            # Here, the `tokenizer.padding_side` doesn't quite matter.
+            target_list.extend(
+                tokenizer.batch_decode(tokens, clean_up_tokenization_spaces=True, skip_special_tokens=True)
+            )
+    return references, full_generations, generations
+
+
 def main(
     # model
     model_name_or_path="gpt2-xl",
-    pad_token="[PAD]",
 
     # data
     train_dataset_path="/home/t-lc/software_v2/GPT3/nlg_datasets/instruct_samsum/train.jsonl",
@@ -163,7 +234,8 @@ def main(
 
     # training
     per_device_train_batch_size=2,
-    per_device_eval_batch_size=2,
+    per_device_eval_batch_size=4,
+    per_device_prompt_batch_size=1,
     gradient_accumulation_steps=256,
     lr=5e-4,
     num_warmup_steps=0,
@@ -194,6 +266,10 @@ def main(
     utils.manual_seed(seed)
 
     tokenizer = transformers.GPT2Tokenizer.from_pretrained(model_name_or_path)
+    model = transformers.GPT2LMHeadModel.from_pretrained(model_name_or_path).to(device)
+    utils.smart_tokenizer_and_embedding_resize(
+        special_tokens_dict=dict(pad_token=PAD_TOKEN), model=model, tokenizer=tokenizer,
+    )
 
     train_dataset = SamsumDataset(tokenizer=tokenizer, path=train_dataset_path, max_size=max_train_size)
     train_dataloader = DataLoader(
@@ -207,16 +283,18 @@ def main(
         eval_dataset,
         batch_size=per_device_eval_batch_size,
         collate_fn=DataCollatorForData2TextLanguageModeling(tokenizer),
-        shuffle=False,
+    )
+    prompt_dataset = SamsumPromptDataset(tokenizer=tokenizer, path=eval_dataset_path, max_size=max_test_size)
+    prompt_dataloader = DataLoader(
+        prompt_dataset,
+        batch_size=per_device_prompt_batch_size,
+        collate_fn=DataCollatorForData2TextLanguageModeling(tokenizer),
     )
 
-    model = transformers.GPT2LMHeadModel.from_pretrained(model_name_or_path).to(device)
-    utils.smart_tokenizer_and_embedding_resize(
-        special_tokens_dict=dict(pad_token=pad_token), model=model, tokenizer=tokenizer
-    )
     # These lines are important for privacy engine to work properly!!!
     private_transformers.lora_utils.convert_gpt2_attention_to_lora(model, lora_r=lora_r, lora_alpha=lora_alpha)
     private_transformers.lora_utils.mark_only_lora_as_trainable(model)
+
     model.train()
     optimizable_params = tuple(param for param in model.parameters() if param.requires_grad)
     optimizer = optim.Adam(params=optimizable_params, lr=lr, weight_decay=weight_decay)
@@ -235,12 +313,13 @@ def main(
             sample_size=len(train_dataset),
             max_grad_norm=max_grad_norm,
             epochs=epochs,
+            accounting_mode="glw",
         )
         privacy_engine.attach(optimizer)
         logging.warning(f'Starting private training: \n{privacy_engine}')
 
     train(
-        model, optimizer, lr_scheduler, train_dataloader, eval_dataloader,
+        model, tokenizer, optimizer, lr_scheduler, train_dataloader, eval_dataloader, prompt_dataloader,
         epochs, gradient_accumulation_steps, non_private
     )
 
