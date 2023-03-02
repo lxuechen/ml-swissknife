97d02a62901ca52c3cf81bd66b5bf3c3ae398f22
Yann Dubois <yanndubois96@gmail.com>
2023-03-01 21:26:53 -0800

    [ENH] add ability for system to have other params

--- ml_swissknife/openai_utils.py
+++ ml_swissknife/openai_utils.py
@@ -71,11 +71,7 @@ def _openai_completion(
     max_batches=sys.maxsize,
     return_text=False,
     **decoding_kwargs,
-) -> Union[
-    Union[StrOrOpenAIObject],
-    Sequence[StrOrOpenAIObject],
-    Sequence[Sequence[StrOrOpenAIObject]],
-]:
+) -> Union[Union[StrOrOpenAIObject], Sequence[StrOrOpenAIObject], Sequence[Sequence[StrOrOpenAIObject]],]:
     """Decode with OpenAI API.
 
     Args:
@@ -138,9 +134,7 @@ def _openai_completion(
                     **decoding_kwargs,
                 )
                 if is_chat:
-                    completion_batch = openai.ChatCompletion.create(
-                        messages=prompt_batch[0], **shared_kwargs
-                    )
+                    completion_batch = openai.ChatCompletion.create(messages=prompt_batch[0], **shared_kwargs)
 
                     choices = completion_batch.choices
                     for choice in choices:
@@ -148,9 +142,7 @@ def _openai_completion(
                         choice["text"] = choice.message.content
 
                 else:
-                    completion_batch = openai.Completion.create(
-                        prompt=prompt_batch, **shared_kwargs
-                    )
+                    completion_batch = openai.Completion.create(prompt=prompt_batch, **shared_kwargs)
                     choices = completion_batch.choices
 
                 for choice in choices:
@@ -160,12 +152,8 @@ def _openai_completion(
             except openai.error.OpenAIError as e:
                 logging.warning(f"OpenAIError: {e}.")
                 if "Please reduce your prompt" in str(e):
-                    batch_decoding_args.max_tokens = int(
-                        batch_decoding_args.max_tokens * 0.8
-                    )
-                    logging.warning(
-                        f"Reducing target length to {batch_decoding_args.max_tokens}, Retrying..."
-                    )
+                    batch_decoding_args.max_tokens = int(batch_decoding_args.max_tokens * 0.8)
+                    logging.warning(f"Reducing target length to {batch_decoding_args.max_tokens}, Retrying...")
                 else:
                     logging.warning("Hit request rate limit; retrying...")
                     time.sleep(sleep_time)  # Annoying rate limit on requests.
@@ -174,32 +162,37 @@ def _openai_completion(
         completions = [completion.text for completion in completions]
     if decoding_args.n > 1:
         # make completions a nested list, where each entry is a consecutive decoding_args.n of original entries.
-        completions = [
-            completions[i : i + decoding_args.n]
-            for i in range(0, len(completions), decoding_args.n)
-        ]
+        completions = [completions[i : i + decoding_args.n] for i in range(0, len(completions), decoding_args.n)]
     if is_single_prompt:
         # Return non-tuple if only 1 input and 1 generation.
         (completions,) = completions
     return completions
 
 
-def prompt_to_chatml(
-    prompt: str, start_token: str = "<|im_start|>", end_token: str = "<|im_end|>"
-):
+def string_to_dict(to_convert):
+    """Converts a string with equal signs to dictionary. E.g.
+    >>> string_to_dict(" name=user university=stanford")
+    {'name': 'user', 'university': 'stanford'}
+    """
+    return {s.split("=", 1)[0]: s.split("=", 1)[1] for s in to_convert.split(" ") if len(s) > 0}
+
+
+def prompt_to_chatml(prompt: str, start_token: str = "<|im_start|>", end_token: str = "<|im_end|>"):
     """Convert a text prompt to ChatML formal
 
     Examples
     --------
-    >>> prompt = "<|im_start|>system\nYou are a helpful assistant.\n<|im_end|>\n<|im_start|>user\nKnock knock.\n<|im_end|>\n<|im_start|>assistant\nWho's there?\n<|im_end|>\n<|im_start|>user\nOrange.\n<|im_end|>"
+    >>> prompt = "<|im_start|>system\nYou are a helpful assistant.\n<|im_end|>\n<|im_start|>system
+    name=example_user\nKnock knock.\n<|im_end|>\n<|im_start|>system name=example_assistant\nWho's
+    there?\n<|im_end|>\n<|im_start|>user\nOrange.\n<|im_end|>"
     >>> print(prompt)
     <|im_start|>system
     You are a helpful assistant.
     <|im_end|>
-    <|im_start|>user
+    <|im_start|>system name=example_user
     Knock knock.
     <|im_end|>
-    <|im_start|>assistant
+    <|im_start|>system name=example_assistant
     Who's there?
     <|im_end|>
     <|im_start|>user
@@ -214,13 +207,25 @@ def prompt_to_chatml(
     prompt = prompt.strip()
     assert prompt.startswith(start_token)
     assert prompt.endswith(end_token)
-    message = [
-        dict(
-            role=p.split("\n", 1)[0].strip(),
-            content=p.split("\n", 1)[1].split(end_token, 1)[0].strip(),
-        )
-        for p in prompt.split("<|im_start|>")[1:]
-    ]
+
+    message = []
+    for p in prompt.split("<|im_start|>")[1:]:
+        newline_splitted = p.split("\n", 1)
+        role = newline_splitted[0].strip()
+        content = newline_splitted[1].split(end_token, 1)[0].strip()
+
+        if role.startswith("system") and role != "system":
+            # based on https://github.com/openai/openai-cookbook/blob/main/examples
+            # /How_to_format_inputs_to_ChatGPT_models.ipynb
+            # and https://github.com/openai/openai-python/blob/main/chatml.md it seems that system can specify a
+            # dictionary of other args
+            other_params = string_to_dict(role.split("system", 1)[-1])
+            role = "system"
+        else:
+            other_params = dict()
+
+        message.append(dict(content=content, role=role, **other_params))
+
     return message
 
 
