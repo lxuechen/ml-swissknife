5bb63951fa0b07b43cfb1ce11974ebe822acfd14
Xuechen Li <12689993+lxuechen@users.noreply.github.com>
2023-05-31 01:33:53 -0700

    doc.

--- upgrade_pypi.sh
+++ upgrade_pypi.sh
@@ -1,4 +1,5 @@
 # Helper to push a new version to pypi.
+# Remember to run `rm -rf ./dist` to remove previous versions; otherwise, those will get re-uploaded.
 python3 -m pip install --upgrade build
 python3 -m build # creates dist/ folder
 python3 -m pip install --upgrade twine
