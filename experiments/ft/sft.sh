89afcc7e7f63ee68b014863b8caf8dc44426d3d1
Xuechen Li <12689993+lxuechen@users.noreply.github.com>
2023-12-19 14:30:09 -0800

    chore: add script

---
+++ experiments/ft/sft.sh
@@ -0,0 +1,19 @@
+python sft.py \
+    --model_name_or_path "microsoft/phi-2" \
+    --bf16 True \
+    --output_dir "/self/scr-ssd/lxuechen/working_dir/phi2" \
+    --num_train_epochs 1 \
+    --per_device_train_batch_size 4 \
+    --per_device_eval_batch_size 4 \
+    --gradient_accumulation_steps 16 \
+    --evaluation_strategy "no" \
+    --save_strategy "steps" \
+    --save_steps 2000 \
+    --save_total_limit 1 \
+    --learning_rate 2e-5 \
+    --weight_decay 0. \
+    --warmup_ratio 0.03 \
+    --lr_scheduler_type "cosine" \
+    --logging_steps 1 \
+    --tf32 True \
+    --cache_dir "/self/scr-ssd/lxuechen/cache"
