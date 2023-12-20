3bb25b56d513de9e866dd0503295ead8bce73196
Xuechen Li <12689993+lxuechen@users.noreply.github.com>
2023-12-19 20:25:12 -0800

    chore: reorg

--- experiments/ft/inference.py
+++ experiments/ft/inference.py
@@ -2,12 +2,41 @@ import fire
 import torch
 import transformers
 from transformers import GenerationConfig
+import datasets
+
+
+def batch_decode(
+    model: transformers.PreTrainedModel,
+    tokenizer: transformers.PreTrainedTokenizer,
+    texts: list[str],
+    batch_size: int = 8,
+):
+    sources = [f"""### Human: {text}\n\n### Assistant:""" for text in texts]
+    encoded = tokenizer.batch_encode_plus(sources, return_tensors="pt")
+    encoded = {key: value.to(model.device) for key, value in encoded.items()}
+
+    output_ids = []
+    for i in range(0, len(texts), batch_size):
+        batch = {key: value[i:i + batch_size] for key, value in encoded.items()}
+        batch_output_ids = model.generate(
+            inputs=batch['input_ids'], attention_mask=batch['attention_mask'],
+            max_length=1000, do_sample=True, top_p=0.95,
+            generation_config=GenerationConfig(eos_token_id=tokenizer.eos_token_id, pad_token_id=tokenizer.pad_token_id)
+        )
+        output_ids.append(batch_output_ids)
+    output_ids = torch.cat(output_ids, dim=0)
+    return tokenizer.batch_decode(output_ids, skip_special_tokens=True)
 
 
 def main(
     model_name_or_path: str = "microsoft/phi-2",
     cache_dir=None,
+    batch_size: int = 8,
 ):
+    dataset = datasets.load_dataset("tatsu-lab/alpaca_eval", split="eval")
+    instructions = dataset['instructions']
+
+    # TODO: Need to do this annoying two-stage load because of issues with phi-2.
     model: transformers.PreTrainedModel = transformers.AutoModelForCausalLM.from_pretrained(
         "microsoft/phi-2", cache_dir=cache_dir, low_cpu_mem_usage=True, device_map="auto",
         trust_remote_code=True, torch_dtype=torch.bfloat16
@@ -18,24 +47,11 @@ def main(
     model.resize_token_embeddings(len(tokenizer))
     model.load_state_dict(torch.load(f"{model_name_or_path}/model.pt"))
 
-    device = next(iter(model.parameters())).device
+    tokenizer.padding_side = "left"
 
-    def decode(text):
-        source = f"""### Human: {text}\n\n### Assistant:"""
-        encoded = tokenizer.batch_encode_plus([source], return_tensors="pt")
-        encoded = {key: value.to(device) for key, value in encoded.items()}
-        output_ids = model.generate(
-            inputs=encoded['input_ids'], attention_mask=encoded['attention_mask'],
-            max_length=1000, do_sample=True, top_p=0.95,
-            generation_config=GenerationConfig(eos_token_id=tokenizer.eos_token_id, pad_token_id=tokenizer.pad_token_id)
-        )
-        return tokenizer.batch_decode(output_ids, skip_special_tokens=True)[0]
-
-    while True:
-        text_in = input("Your query:")
-        text_out = decode(text_in)
-        print(text_out)
-        print('--------')
+    instructions = instructions[:10]
+    texts = batch_decode(model, tokenizer, instructions, batch_size=batch_size)
+    print(texts)
 
 
 if __name__ == "__main__":
--- experiments/ft/sft.sh
+++ experiments/ft/sft.sh
@@ -2,7 +2,7 @@ python sft.py \
     --model_name_or_path "microsoft/phi-2" \
     --bf16 True \
     --output_dir "/self/scr-ssd/lxuechen/working_dir/phi2" \
-    --num_train_epochs 0.1 \
+    --num_train_epochs 2 \
     --per_device_train_batch_size 4 \
     --per_device_eval_batch_size 4 \
     --gradient_accumulation_steps 16 \
