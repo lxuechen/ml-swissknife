3e573708b2707e239e4fb91c81b53c28ae01947b
Xuechen Li <12689993+lxuechen@users.noreply.github.com>
2023-12-20 15:04:50 -0800

    chore: fix scripts

---
+++ experiments/ft/scripts/dpo.sh
@@ -0,0 +1,21 @@
+python dpo.py \
+    --model_name_or_path "/self/scr-ssd/lxuechen/working_dir/phi-2-sft" \
+    --bf16 True \
+    --output_dir "/self/scr-ssd/lxuechen/working_dir/phi-2-dpo" \
+    --num_train_epochs 0.2 \
+    --per_device_train_batch_size 4 \
+    --per_device_eval_batch_size 4 \
+    --gradient_accumulation_steps 16 \
+    --evaluation_strategy "no" \
+    --save_strategy "steps" \
+    --save_steps 2000 \
+    --save_total_limit 1 \
+    --learning_rate 1e-6 \
+    --beta 0.1 \
+    --weight_decay 0. \
+    --warmup_ratio 0.03 \
+    --lr_scheduler_type "cosine" \
+    --logging_steps 1 \
+    --tf32 True \
+    --cache_dir "/self/scr-ssd/lxuechen/cache" \
+    --save_raw_state_dict True
