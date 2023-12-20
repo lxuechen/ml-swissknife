3c08852e3e9955976f73b49c9e6845cea31cc7da
Xuechen Li <12689993+lxuechen@users.noreply.github.com>
2023-12-20 15:34:12 -0800

    chore: model weights in fp32

--- experiments/ft/dpo.py
+++ experiments/ft/dpo.py
@@ -252,7 +252,6 @@ def main():
         low_cpu_mem_usage=True,
         device_map="auto",
         trust_remote_code=True,
-        torch_dtype=torch.bfloat16,
     )
     model.resize_token_embeddings(len(tokenizer))
     model.load_state_dict(torch.load(f"{model_args.model_name_or_path}/model.pt"))
