1055a65e2dcf9350cc919ac5cb50054d62d1e830
Xuechen Li <12689993+lxuechen@users.noreply.github.com>
2024-01-09 16:02:58 -0800

    chore: revise

--- experiments/ft/play_mixtral.py
+++ experiments/ft/play_mixtral.py
@@ -1,28 +1,57 @@
+import fire
 import torch
 from transformers import AutoModelForCausalLM, AutoTokenizer
 
-cache_dir = "/self/scr-ssd/lxuechen/cache"
-device = "cuda"  # the device to load the model onto
 
-model = AutoModelForCausalLM.from_pretrained(
-    "mistralai/Mixtral-8x7B-v0.1",
-    cache_dir=cache_dir,
-    torch_dtype=torch.float16,
-    device_map="auto",
-    low_cpu_mem_usage=True,
-)
-tokenizer = AutoTokenizer.from_pretrained("mistralai/Mixtral-8x7B-v0.1", cache_dir=cache_dir)
-num_params = sum(p.numel() for p in model.parameters())
-print(model)
-print(num_params // 10 ** 9)
-for p in model.parameters():
-    print(p.device)
-breakpoint()
+def sample(**kwargs):
+    cache_dir = "/self/scr-ssd/lxuechen/cache"
+    device = "cuda"  # the device to load the model onto
 
-prompt = "My favourite condiment is"
+    model = AutoModelForCausalLM.from_pretrained(
+        "mistralai/Mixtral-8x7B-v0.1",
+        cache_dir=cache_dir,
+        torch_dtype=torch.float16,
+        device_map="auto",
+        low_cpu_mem_usage=True,
+        attn_implementation="flash_attention_2",
+    )
+    tokenizer = AutoTokenizer.from_pretrained("mistralai/Mixtral-8x7B-v0.1", cache_dir=cache_dir)
+    num_params = sum(p.numel() for p in model.parameters())
+    print(model)
+    print(num_params // 10 ** 9)
+    for p in model.parameters():
+        print(p.device)
+    breakpoint()
 
-with torch.no_grad():
-    model_inputs = tokenizer([prompt], return_tensors="pt").to(device)
-    generated_ids = model.generate(**model_inputs, max_new_tokens=100, do_sample=True)
-    text = tokenizer.batch_decode(generated_ids)[0]
-print(text)
+    prompt = "My favourite condiment is"
+
+    with torch.no_grad():
+        model_inputs = tokenizer([prompt], return_tensors="pt").to(device)
+        generated_ids = model.generate(**model_inputs, max_new_tokens=100, do_sample=True)
+        text = tokenizer.batch_decode(generated_ids)[0]
+    print(text)
+
+
+def check_params(**kwargs):
+    cache_dir = "/self/scr-ssd/lxuechen/cache"
+    device = "cuda"  # the device to load the model onto
+
+    model = AutoModelForCausalLM.from_pretrained(
+        "mistralai/Mixtral-8x7B-v0.1",
+        cache_dir=cache_dir,
+        torch_dtype=torch.float16,
+        device_map="auto",
+        low_cpu_mem_usage=True,
+        attn_implementation="flash_attention_2",
+    )
+
+
+def main(task, **kwargs):
+    if task == "sample":
+        sample(**kwargs)
+    elif task == "check_params":
+        check_params(**kwargs)
+
+
+if __name__ == "__main__":
+    fire.Fire(main)
